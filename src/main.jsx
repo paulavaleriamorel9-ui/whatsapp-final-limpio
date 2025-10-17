35168e8997db76dd5b99e236df4cd01519866587
Paula <paulavaleriamorel9@gmail.com>
2025-10-16 21:52:46 -0300

    Fix: Usando HashRouter en main.jsx para deploy en hosting estático

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import './styles.css';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import '\.\/styles\.css';
+
+import { HashRouter } from 'react-router-dom'; 
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+  
+    <HashRouter> 
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
